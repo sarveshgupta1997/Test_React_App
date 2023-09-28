9e934d8578438f82c44576b4bc34fdd52cbf1e99
sarveshguptanative <sarvesh.gupta@nativebyte.in>
2023-09-28 11:43:44 +0530

    test3 commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sup..?
+          Sup...?
         </p>
         <a
           className="App-link"
